96094ec1280e71c295536ea6c4efb1a3baf6b274
Dmitry <send.dmitry@mail.ru>
2023-12-19 22:52:33 +0400

    One commit

---
+++ bot.py
@@ -0,0 +1,59 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from keyboard import create_main_keyboard
+from bd_config import User, Session
+
+
+BOT_TOKEN = "6481132072:AAEcFRg5bH9eEgzUXHMdyzOkdTHClDAqrfw"
+FLASK_URL = 'https://bbb5-94-231-133-134.ngrok-free.app'
+bot = Bot(BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher()
+
+
+#sjdvcj
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name
+    main_kb = create_main_keyboard(user_id, FLASK_URL)
+
+    # Проверяем, существует ли пользователь в базе данных
+    if not user_exists(user_id):
+        # Если пользователя нет, сохраняем его в базе данных
+        save_user(user_id, first_name)
+        await message.reply("Поздравляем! Теперь вы базе данных!🤡")
+    else:
+        await message.reply("Вы уже наш чел!👌")
+
+    await message.answer(f'Привет, {first_name}!😘', reply_markup=main_kb)
+
+
+# Функция для проверки существования пользователя в базе данных
+def user_exists(user_id):
+    session = Session()
+    user = session.query(User).filter_by(user_id=user_id).first()
+    session.close()
+    return user is not None
+
+
+# Функция для сохранения пользователя в базе данных
+def save_user(user_id, first_name):
+    session = Session()
+    new_user = User(user_id=user_id, user_name=first_name)
+    session.add(new_user)
+    session.commit()
+    session.close()
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ static/scripts.js
@@ -0,0 +1,72 @@
+let now = new Date();
+let month = now.getMonth() + 1;
+let year = now.getFullYear();
+let day = now.getDate();
+let mydate = year + '-' + month + '-' + day;
+document.getElementById('date-task').value = mydate;
+document.getElementById('daily-time').value = new Date().toTimeString().slice(0,5);
+document.getElementById('once-time').value = new Date().toTimeString().slice(0,5);
+
+
+const textarea = document.querySelector('textarea');
+const input_text = document.querySelector('input');
+const counter = document.querySelector('.current');
+const counter_name = document.querySelector('.current-name');
+const div_counter_name = document.querySelector('.counter-name')
+const div_counter_task_box = document.querySelector('.counter-task-box')
+
+
+textarea.addEventListener('input', onInput)
+function onInput(event) {
+const counter_task_box_length = event.target.value.length;
+counter.textContent = counter_task_box_length;
+if (counter_task_box_length == 250) {
+    div_counter_task_box.style.color = '#bd4242'
+}else{
+    div_counter_task_box.style.color = '#C8C8D2'
+}
+}
+
+
+input_text.addEventListener('input', onInput_name)
+function onInput_name(event) {
+const counter_task_name_length = event.target.value.length;
+counter_name.textContent = counter_task_name_length;
+if (counter_task_name_length == 90) {
+    div_counter_name.style.color = '#bd4242'
+}else{
+    div_counter_name.style.color = '#C8C8D2'
+}
+}
+
+
+let checkbox = document.querySelector('.checkbox-inp');
+let notifications = document.querySelector('.notifications');
+
+checkbox.addEventListener('change', function () {
+if (checkbox.checked) {
+    notifications.classList.add('show');
+} else {
+    notifications.classList.remove('show');
+}
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+let radioButtons = document.querySelectorAll('.radio-priority input[type="radio"]');
+let priorityTaskBlock = document.querySelector('.priority-task');
+
+
+radioButtons.forEach(function (radioButton) {
+radioButton.addEventListener('change', function () {
+    let anyChecked = Array.from(radioButtons).some(function (rb) {
+        return rb.checked;
+    });
+
+    if (anyChecked) {
+        priorityTaskBlock.style.backgroundImage = 'none';
+    } else {
+        priorityTaskBlock.style.backgroundImage = 'url("star.svg")';
+    }
+});
+});
+});
